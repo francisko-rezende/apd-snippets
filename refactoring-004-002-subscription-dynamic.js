b52b1e2b113d8cd831eb60b01c5392a7e89e1163
filipenevola <filipe@quave.com.br>
2024-03-23 10:18:17 -0400

    refactoring-004-002-subscription-dynamic

---
+++ refactoring-004-002-subscription-dynamic.js
@@ -0,0 +1,13 @@
+const mySubscriptions = SubscriptionsCollection.find({ userId });
+
+const SubscriptionStatuses = JSON.parse(
+    fs.readFileSync('refactoring-004-002-subscription-dynamic.json')
+);
+
+const mySubscriptionsStatuses = mySubscriptions.map(({ statusValue }) =>
+    Object.values(SubscriptionStatuses).find(({ value }) => value === statusValue)
+);
+
+const hasValidSubscription = mySubscriptions.some(({ statusValue }) =>
+    SubscriptionStatuses[statusValue].isValid
+);
