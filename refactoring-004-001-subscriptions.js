03098c15320fc7005a2cbe45dfafaeb6911008fb
filipenevola <filipe@quave.com.br>
2024-03-23 10:15:12 -0400

    refactoring-004-001-subscriptions

---
+++ refactoring-004-001-subscriptions.js
@@ -0,0 +1,16 @@
+const mySubscriptions = SubscriptionsCollection.find({ userId });
+
+const SubscriptionStatuses = {
+    ACTIVE: { value: 'ACTIVE', isValid: true },
+    PAID: { value: 'ACTIVE', isValid: true },
+    PENDING: { value: 'PENDING' },
+    CANCELED: { value: 'CANCELED' },
+};
+
+const mySubscriptionsStatuses = mySubscriptions.map(({ statusValue }) =>
+    Object.values(SubscriptionStatuses).find(({ value }) => value === statusValue)
+);
+
+const hasValidSubscription = mySubscriptions.some(({ statusValue }) =>
+    SubscriptionStatuses[statusValue].isValid
+);
